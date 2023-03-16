2a075a7bce97cd0b65bd2c778502d596244512cd
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-03-17 01:42:11 +0200

    + 1 task Kyu 7

---
+++ src/main/java/org/example/kyu_7/ReverseWords.java
@@ -0,0 +1,22 @@
+package org.example.kyu_7;
+/**
+ Complete the function that accepts a string parameter, and reverses each word in the string.
+ All spaces in the string should be retained.
+
+ Examples
+ "This is an example!" ==> "sihT si na !elpmaxe"
+ "double  spaces"      ==> "elbuod  secaps"
+ */
+public class ReverseWords
+{
+  public static String reverseWords(final String original) {
+    String[] split = original.split(" ");
+    int i = 0;
+    for(String str : split) {
+      split[i] = new StringBuilder(str).reverse().toString();
+      i++;
+    }
+    return String.join(" ", split);
+
+  }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,15 @@
+import org.example.kyu_7.ReverseWords;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void exampleCases() {
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", ReverseWords.reverseWords("The quick brown fox jumps over the lazy dog."));
+        assertEquals("elppa", ReverseWords.reverseWords("apple"));
+        assertEquals("a b c d", ReverseWords.reverseWords("a b c d"));
+        assertEquals("elbuod  decaps  sdrow", ReverseWords.reverseWords("double  spaced  words"));
+    }
+}
